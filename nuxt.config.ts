b39868af9804af309daca365d150663182e385de
Gideon Assafuah <gassafuah@odoogap.com>
2024-11-07 00:17:32 -0500

    fetch products from odoo to product.json for algolia search

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   app: {
     head: {
       viewport: "minimum-scale=1, initial-scale=1, width=device-width",
@@ -15,6 +16,7 @@ export default defineNuxtConfig({
   robots: {
     allow: "/category/*",
   },
+
   extends: [
     "./domains/auth",
     "./domains/recent-view-products",
@@ -25,10 +27,11 @@ export default defineNuxtConfig({
     "./domains/core",
     "./domains/my-account",
     "./domains/product",
-    //"./domains/search-algolia",
-    "./domains/search-default",
+    "./domains/search-algolia",
+    //"./domains/search-default",
     "./domains/wishlist",
   ],
+
   modules: [
     "@pinia/nuxt",
     "@nuxtjs/tailwindcss",
@@ -45,6 +48,7 @@ export default defineNuxtConfig({
     "nuxt-delay-hydration",
     "nuxt-typed-router",
   ],
+
   runtimeConfig: {
     shouldByPassCacheQueryNames: [
       "LoadCartQuery",
@@ -60,11 +64,13 @@ export default defineNuxtConfig({
       currencyPrecision: "",
     },
   },
+
   googleFonts: {
     families: {
       "Red Hat Display": [400, 500, 700],
     },
   },
+
   i18n: {
     locales: [
       {
@@ -77,14 +83,17 @@ export default defineNuxtConfig({
     langDir: "lang",
     defaultLocale: "en",
   },
+
   delayHydration: {
     mode: "init",
   },
+
   vite: {
     optimizeDeps: {
       include: ["lodash-es"],
     },
   },
+
   build: {
     transpile: [
       "tslib",
@@ -97,6 +106,7 @@ export default defineNuxtConfig({
       "@erpgap/odoo-sdk-api-client",
     ],
   },
+
   image: {
     providers: {
       odooProvider: {
@@ -114,11 +124,13 @@ export default defineNuxtConfig({
       xs: 376,
     },
   },
+
   routeRules: {
     "/": { swr: Number(process.env?.NUXT_SWR_CACHE_TIME) },
     "/category/*": { swr: Number(process.env?.NUXT_SWR_CACHE_TIME) },
     "/product/*": { swr: Number(process.env?.NUXT_SWR_CACHE_TIME) },
   },
+
   nitro: {
     // compressPublicAssets: true,
     storage: {
@@ -134,19 +146,25 @@ export default defineNuxtConfig({
       },
     },
   },
+
   site: {
     url: "https://vsfsdk.labs.odoogap.com/",
     name: "ERPGAP VSF",
     description: "Welcome to an awesome ecommerce site!",
     defaultLocale: "en",
   },
+
   tailwindcss: {
     viewer: false,
   },
+
   device: {
     refreshOnResize: true,
   },
+
   experimental: {
     asyncContext: false,
   },
+
+  compatibilityDate: "2024-11-06",
 });
