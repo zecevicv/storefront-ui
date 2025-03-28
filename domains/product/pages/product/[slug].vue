c5bcc224617d80a8a2794557056e4effb637f1fe
soareslssa <soareslssa.dev@gmail.com>
2025-03-28 10:00:27 -0300

    fix: 🐛 breadcrumb product current page

--- domains/product/pages/product/[slug].vue
+++ domains/product/pages/product/[slug].vue
@@ -15,15 +15,15 @@ import {
   SfLink,
   SfRating,
   SfThumbnail,
-} from '@storefront-ui/vue'
-import type { LocationQueryRaw } from 'vue-router'
-import type { OrderLine, Product } from '~/graphql'
-import generateSeo, { type SeoEntity } from '~/utils/buildSEOHelper'
+} from "@storefront-ui/vue";
+import type { LocationQueryRaw } from "vue-router";
+import type { OrderLine, Product } from "~/graphql";
+import generateSeo, { type SeoEntity } from "~/utils/buildSEOHelper";
 
-const route = useRoute()
+const route = useRoute();
 
-const cleanPath = computed(() => route?.path?.replace(/\/$/, ''))
-const cleanFullPath = computed(() => route?.fullPath?.replace(/\/$/, ''))
+const cleanPath = computed(() => route?.path?.replace(/\/$/, ""));
+const cleanFullPath = computed(() => route?.fullPath?.replace(/\/$/, ""));
 
 const {
   loadProductTemplate,
@@ -32,7 +32,7 @@ const {
   getAllColors,
   getAllMaterials,
   getAllSizes,
-} = useProductTemplate(cleanPath.value)
+} = useProductTemplate(cleanPath.value);
 const {
   loadProductVariant,
   loadingProductVariant,
@@ -41,103 +41,102 @@ const {
   breadcrumbs,
   getRegularPrice,
   getSpecialPrice,
-} = useProductVariant(cleanFullPath.value)
-const { addProductToRecentViews } = useRecentViewProducts()
-const { wishlistAddItem, isInWishlist, wishlistRemoveItem } = useWishlist()
-const { cart, cartAdd } = useCart()
+} = useProductVariant(cleanFullPath.value);
+const { addProductToRecentViews } = useRecentViewProducts();
+const { wishlistAddItem, isInWishlist, wishlistRemoveItem } = useWishlist();
+const { cart, cartAdd } = useCart();
 
-useHead(generateSeo<SeoEntity>(productVariant.value, 'Product'))
+useHead(generateSeo<SeoEntity>(productVariant.value, "Product"));
 
 const params = computed(() => ({
-  combinationId: Object.values(route.query)?.map(value =>
-    parseInt(value as string),
+  combinationId: Object.values(route.query)?.map((value) =>
+    parseInt(value as string)
   ),
   productTemplateId: productTemplate?.value?.id,
-}))
+}));
 
 const selectedSize = computed(() =>
-  route.query.Size ? Number(route.query.Size) : getAllSizes?.value?.[0]?.value,
-)
+  route.query.Size ? Number(route.query.Size) : getAllSizes?.value?.[0]?.value
+);
 
 const selectedColor = computed(() =>
   route.query.Color
     ? Number(route.query.Color)
-    : getAllColors?.value?.[0]?.value,
-)
+    : getAllColors?.value?.[0]?.value
+);
 
 const selectedMaterial = computed(() =>
   route.query.Material
     ? Number(route.query.Material)
-    : getAllMaterials?.value?.[0]?.value,
-)
+    : getAllMaterials?.value?.[0]?.value
+);
 
-const productDetailsOpen = ref(true)
-const quantitySelectorValue = ref(1)
+const productDetailsOpen = ref(true);
+const quantitySelectorValue = ref(1);
 
 const updateFilter = async (filter: LocationQueryRaw | undefined) => {
-  const query: LocationQueryRaw = {}
+  const query: LocationQueryRaw = {};
 
   if (selectedMaterial.value && selectedMaterial.value !== 0) {
-    query.Material = selectedMaterial.value
+    query.Material = selectedMaterial.value;
   }
 
   if (selectedColor.value && selectedColor.value !== 0) {
-    query.Color = selectedColor.value
+    query.Color = selectedColor.value;
   }
 
   if (selectedSize.value && selectedSize.value !== 0) {
-    query.Size = selectedSize.value
+    query.Size = selectedSize.value;
   }
 
-  Object.assign(query, filter)
-  await navigateTo({ query })
-}
+  Object.assign(query, filter);
+  await navigateTo({ query });
+};
 
 const tomorrow = computed(() => {
-  const date = new Date()
-  date.setDate(date.getDate() + 1)
-  return date.toDateString().slice(0, 10)
-})
+  const date = new Date();
+  date.setDate(date.getDate() + 1);
+  return date.toDateString().slice(0, 10);
+});
 
 const productsInCart = computed(() => {
   return (
     cart.value?.order?.orderLines?.find(
       (orderLine: OrderLine) =>
-        orderLine.product?.id === productVariant?.value.id,
+        orderLine.product?.id === productVariant?.value.id
     )?.quantity || 0
-  )
-})
+  );
+});
 
 const handleCartAdd = async () => {
-  let id = productVariant?.value.id
+  let id = productVariant?.value.id;
   if (!productVariant.value.combinationInfoVariant) {
-    id = Number(productVariant?.value.firstVariant?.id)
+    id = Number(productVariant?.value.firstVariant?.id);
   }
-  await cartAdd(id, quantitySelectorValue.value)
-}
+  await cartAdd(id, quantitySelectorValue.value);
+};
 
 const handleWishlistAddItem = async (firstVariant: Product) => {
-  await wishlistAddItem(firstVariant.id)
-}
+  await wishlistAddItem(firstVariant.id);
+};
 
 const handleWishlistRemoveItem = async (firstVariant: Product) => {
-  await wishlistRemoveItem(firstVariant.id)
-}
+  await wishlistRemoveItem(firstVariant.id);
+};
 
-addProductToRecentViews(productTemplate.value?.id)
+addProductToRecentViews(productTemplate.value?.id);
 
-await loadProductTemplate({ slug: cleanPath.value })
-await loadProductVariant(params.value)
+await loadProductTemplate({ slug: cleanPath.value });
+await loadProductVariant(params.value);
 </script>
 
 <template>
   <NuxtErrorBoundary>
     <div v-if="productTemplate?.id && !loadingProductTemplate">
-      <UiBreadcrumb
-        :breadcrumbs="breadcrumbs"
-        class="self-start mt-5 mb-10 cursor-pointer"
-      />
-      <div class="md:grid grid-areas-product-page grid-cols-product-page gap-x-6">
+      <UiBreadcrumb :breadcrumbs="breadcrumbs" class="self-start mt-5 mb-10" />
+      <div
+        class="md:grid grid-areas-product-page grid-cols-product-page gap-x-6"
+      >
         <section class="grid-in-left-top md:h-full xl:max-h-[700px]">
           <LazyUiGallery :images="getImages" />
         </section>
@@ -149,11 +148,7 @@ await loadProductVariant(params.value)
             <div
               class="inline-flex items-center justify-center font-medium rounded-none bg-secondary-800 text-sm p-1.5 gap-1 mb-4"
             >
-              <SfIconSell
-                color="white"
-                size="sm"
-                class="mr-1"
-              />
+              <SfIconSell color="white" size="sm" class="mr-1" />
               <span class="mr-1 text-white">{{ $t(`sale`) }}</span>
             </div>
             <h1
@@ -164,8 +159,8 @@ await loadProductVariant(params.value)
             </h1>
             <div
               v-if="
-                productVariant
-                  && productVariant?.combinationInfoVariant?.has_discounted_price
+                productVariant &&
+                productVariant?.combinationInfoVariant?.has_discounted_price
               "
               class="my-1"
             >
@@ -179,10 +174,7 @@ await loadProductVariant(params.value)
                 {{ $currency(getRegularPrice) }}
               </span>
             </div>
-            <div
-              v-else
-              class="my-1"
-            >
+            <div v-else class="my-1">
               <span
                 class="mr-2 text-secondary-700 font-bold font-headings text-2xl"
                 data-testid="price"
@@ -191,17 +183,8 @@ await loadProductVariant(params.value)
               </span>
             </div>
             <div class="inline-flex items-center mt-4 mb-2">
-              <SfRating
-                size="xs"
-                :value="4"
-                :max="5"
-              />
-              <SfCounter
-                class="ml-1"
-                size="xs"
-              >
-                26
-              </SfCounter>
+              <SfRating size="xs" :value="4" :max="5" />
+              <SfCounter class="ml-1" size="xs"> 26 </SfCounter>
               <SfLink
                 href="#"
                 variant="secondary"
@@ -240,7 +223,7 @@ await loadProductVariant(params.value)
                   <template #prefix>
                     <SfIconShoppingCart size="sm" />
                   </template>
-                  {{ $t('addToCart') }}
+                  {{ $t("addToCart") }}
                 </SfButton>
               </div>
               <div class="flex justify-center mt-4 gap-x-4">
@@ -248,7 +231,8 @@ await loadProductVariant(params.value)
                   type="button"
                   size="sm"
                   variant="tertiary"
-                  :class="productVariant?.isInWishlist ? 'bg-primary-100' : 'bg-white'
+                  :class="
+                    productVariant?.isInWishlist ? 'bg-primary-100' : 'bg-white'
                   "
                   @click="
                     isInWishlist(productVariant?.id as number)
@@ -278,22 +262,13 @@ await loadProductVariant(params.value)
                 class="flex-shrink-0 mr-1 text-neutral-500"
               />
               <p class="text-sm">
-                <i18n-t
-                  keypath="additionalInfo.shipping"
-                  scope="global"
-                >
+                <i18n-t keypath="additionalInfo.shipping" scope="global">
                   <template #date>
                     {{ tomorrow }}
                   </template>
                   <template #addAddress>
-                    <SfLink
-                      class="ml-1"
-                      href="#"
-                      variant="secondary"
-                    >
-                      {{
-                        $t('additionalInfo.addAddress')
-                      }}
+                    <SfLink class="ml-1" href="#" variant="secondary">
+                      {{ $t("additionalInfo.addAddress") }}
                     </SfLink>
                   </template>
                 </i18n-t>
@@ -305,19 +280,10 @@ await loadProductVariant(params.value)
                 class="flex-shrink-0 mr-1 text-neutral-500"
               />
               <p class="text-sm">
-                <i18n-t
-                  keypath="additionalInfo.pickup"
-                  scope="global"
-                >
+                <i18n-t keypath="additionalInfo.pickup" scope="global">
                   <template #checkAvailability>
-                    <SfLink
-                      class="ml-1"
-                      href="#"
-                      variant="secondary"
-                    >
-                      {{
-                        $t('additionalInfo.checkAvailability')
-                      }}
+                    <SfLink class="ml-1" href="#" variant="secondary">
+                      {{ $t("additionalInfo.checkAvailability") }}
                     </SfLink>
                   </template>
                 </i18n-t>
@@ -328,19 +294,10 @@ await loadProductVariant(params.value)
                 size="sm"
                 class="flex-shrink-0 mr-1 text-neutral-500"
               />
-              <i18n-t
-                keypath="additionalInfo.returns"
-                scope="global"
-              >
+              <i18n-t keypath="additionalInfo.returns" scope="global">
                 <template #details>
-                  <SfLink
-                    class="ml-1"
-                    href="#"
-                    variant="secondary"
-                  >
-                    {{
-                      $t('additionalInfo.details')
-                    }}
+                  <SfLink class="ml-1" href="#" variant="secondary">
+                    {{ $t("additionalInfo.details") }}
                   </SfLink>
                 </template>
               </i18n-t>
@@ -349,15 +306,14 @@ await loadProductVariant(params.value)
         </section>
         <section class="grid-in-left-bottom md:mt-8">
           <UiDivider class="mt-10 mb-6" />
-          <div
-            class="lg:px-4"
-            data-testid="product-properties"
-          >
+          <div class="lg:px-4" data-testid="product-properties">
             <fieldset
               v-if="getAllSizes && getAllSizes?.length"
               class="pb-4 flex"
             >
-              <legend class="block mb-2 text-base font-medium leading-6 text-neutral-900">
+              <legend
+                class="block mb-2 text-base font-medium leading-6 text-neutral-900"
+              >
                 Size
               </legend>
               <span
@@ -374,8 +330,8 @@ await loadProductVariant(params.value)
                   }"
                   :model-value="value == selectedSize"
                   @update:model-value="
-                    value != selectedSize
-                      && updateFilter({ ['Size']: value.toString() })
+                    value != selectedSize &&
+                      updateFilter({ ['Size']: value.toString() })
                   "
                 >
                   {{ label }}
@@ -386,7 +342,9 @@ await loadProductVariant(params.value)
               v-if="getAllMaterials && getAllMaterials?.length"
               class="pb-4 flex"
             >
-              <legend class="block mb-2 text-base font-medium leading-6 text-neutral-900">
+              <legend
+                class="block mb-2 text-base font-medium leading-6 text-neutral-900"
+              >
                 Material
               </legend>
               <span
@@ -404,8 +362,8 @@ await loadProductVariant(params.value)
                   }"
                   :model-value="value == selectedMaterial"
                   @update:model-value="
-                    value != selectedMaterial
-                      && updateFilter({ ['Material']: value.toString() })
+                    value != selectedMaterial &&
+                      updateFilter({ ['Material']: value.toString() })
                   "
                 >
                   {{ label }}
@@ -416,7 +374,9 @@ await loadProductVariant(params.value)
               v-if="getAllColors && getAllColors?.length"
               class="pb-2 flex"
             >
-              <legend class="block mb-2 text-base font-medium leading-6 text-neutral-900">
+              <legend
+                class="block mb-2 text-base font-medium leading-6 text-neutral-900"
+              >
                 Color
               </legend>
               <span
@@ -434,15 +394,12 @@ await loadProductVariant(params.value)
                   }"
                   :model-value="value == selectedColor"
                   @update:model-value="
-                    value != selectedColor
-                      && updateFilter({ ['Color']: value.toString() })
+                    value != selectedColor &&
+                      updateFilter({ ['Color']: value.toString() })
                   "
                 >
                   <template #prefix>
-                    <SfThumbnail
-                      size="sm"
-                      :style="{ background: label }"
-                    />
+                    <SfThumbnail size="sm" :style="{ background: label }" />
                   </template>
                   {{ label }}
                 </SfChip>
@@ -456,8 +413,10 @@ await loadProductVariant(params.value)
               summary-class="md:rounded-md w-full hover:bg-neutral-100 py-2 lg:pl-4 pr-3 flex justify-between items-center"
             >
               <template #summary>
-                <h2 class="font-bold font-headings text-lg leading-6 md:text-2xl">
-                  {{ $t('productDetails') }}
+                <h2
+                  class="font-bold font-headings text-lg leading-6 md:text-2xl"
+                >
+                  {{ $t("productDetails") }}
                 </h2>
               </template>
               <p>
@@ -469,8 +428,10 @@ await loadProductVariant(params.value)
               summary-class="md:rounded-md w-full hover:bg-neutral-100 py-2 lg:pl-4 pr-3 flex justify-between items-center"
             >
               <template #summary>
-                <h2 class="font-bold font-headings text-lg leading-6 md:text-2xl">
-                  {{ $t('customerReviews') }}
+                <h2
+                  class="font-bold font-headings text-lg leading-6 md:text-2xl"
+                >
+                  {{ $t("customerReviews") }}
                 </h2>
               </template>
               <p>
@@ -495,7 +456,7 @@ await loadProductVariant(params.value)
           height="300"
         />
         <p class="mt-8 font-medium">
-          {{ $t('emptyStateText') }}
+          {{ $t("emptyStateText") }}
         </p>
       </div>
     </template>
