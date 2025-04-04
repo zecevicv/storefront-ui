3dafaddcc671529cc8b072b8cea5f0b9d4ee7396
Lucas Soares <soareslssa.dev@gmail.com>
2025-04-04 15:29:38 -0300

    fix: 🐛 core composables directory naming

--- domains/checkout/pages/checkout.vue
+++ domains/checkout/pages/checkout.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { useCountryList } from '~/domains/core/composable/useCountryList'
+
 import { AddressEnum, type Partner } from '~/graphql'
 
 const { cart, loadCart, totalItemsInCart } = useCart()
@@ -25,28 +25,17 @@ if (totalItemsInCart?.value === 0) {
         <div class="col-span-7 mb-10 md:mb-0">
           <UiDivider class="w-screen md:w-auto -mx-4 md:mx-0" />
 
-          <LazyCheckoutContactInformation
-            v-if="cart?.order?.partner"
-            :heading="$t('contactInfo.heading')"
-            :partner-data="cart?.order?.partner as Partner"
-          />
+          <LazyCheckoutContactInformation v-if="cart?.order?.partner" :heading="$t('contactInfo.heading')"
+            :partner-data="cart?.order?.partner as Partner" />
 
           <UiDivider class="w-screen md:w-auto -mx-4 md:mx-0" />
-          <LazyCheckoutAddressForm
-            :heading="$t('shipping.heading')"
-            :description="$t('shipping.description')"
-            :button-text="$t('shipping.addButton')"
-            :type="AddressEnum.Shipping"
-            :saved-address="cart.order?.partnerShipping as Partner"
-          />
+          <LazyCheckoutAddressForm :heading="$t('shipping.heading')" :description="$t('shipping.description')"
+            :button-text="$t('shipping.addButton')" :type="AddressEnum.Shipping"
+            :saved-address="cart.order?.partnerShipping as Partner" />
           <UiDivider class="w-screen md:w-auto -mx-4 md:mx-0" />
-          <LazyCheckoutAddressForm
-            :heading="$t('billing.heading')"
-            :description="$t('billing.description')"
-            :button-text="$t('billing.addButton')"
-            :type="AddressEnum.Billing"
-            :saved-address="cart.order?.partnerInvoice as Partner"
-          />
+          <LazyCheckoutAddressForm :heading="$t('billing.heading')" :description="$t('billing.description')"
+            :button-text="$t('billing.addButton')" :type="AddressEnum.Billing"
+            :saved-address="cart.order?.partnerInvoice as Partner" />
 
           <UiDivider class-name="w-screen md:w-auto -mx-4 md:mx-0" />
 
