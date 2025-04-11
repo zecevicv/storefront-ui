20591f55ba6f126d4f80d120440ec2908a936994
Leonardo Muniz <lmuniz@odoogap.com>
2025-04-11 20:12:31 -0300

    fix: 🐛 custom sf select

--- domains/core/components/ui/form/CustomSfSelect.vue
+++ domains/core/components/ui/form/CustomSfSelect.vue
@@ -51,41 +51,67 @@ const modelProxy = computed({
 </script>
 
 <template>
-  <span :class="['relative flex flex-col rounded-md', {
-    'focus-within:outline focus-within:outline-offset': isFocusVisible,
-  },
-    wrapperClassName,
-  ]" data-testid="select">
-    <select v-model="modelProxy" :required="required" :disabled="disabled" :class="[
-      'appearance-none disabled:cursor-not-allowed cursor-pointer pl-4 pr-3.5 text-neutral-900 ring-inset focus:ring-primary-700 focus:ring-2 outline-none bg-transparent rounded-md ring-1 ring-neutral-300 hover:ring-primary-700 active:ring-2 active:ring-primary-700 disabled:bg-disabled-100 disabled:text-disabled-900 disabled:ring-disabled-200',
-      {
-        'py-1.5': size === SfSelectSize.sm,
-        'py-2': size === SfSelectSize.base,
-        'py-3 text-base': size === SfSelectSize.lg,
-        '!ring-negative-700 ring-2': invalid && !disabled,
-      },
-    ]" data-testid="select-input" v-bind="$attrs" @blur="close" @change="close" @click="open" @keydown.space="open">
-      <option v-if="placeholder" disabled hidden class="text-sm bg-neutral-300" value="" :class="[
-        'bg-neutral-300 text-sm',
+  <span
+    :class="['relative flex flex-col rounded-md', {
+               'focus-within:outline focus-within:outline-offset': isFocusVisible,
+             },
+             wrapperClassName,
+    ]"
+    data-testid="select"
+  >
+    <select
+      v-model="modelProxy"
+      :required="required"
+      :disabled="disabled"
+      :class="[
+        'appearance-none disabled:cursor-not-allowed cursor-pointer pl-4 pr-3.5 text-neutral-900 ring-inset focus:ring-primary-700 focus:ring-2 outline-none bg-transparent rounded-md ring-1 ring-neutral-300 hover:ring-primary-700 active:ring-2 active:ring-primary-700 disabled:bg-disabled-100 disabled:text-disabled-900 disabled:ring-disabled-200',
         {
-          'text-base': size === SfSelectSize.lg,
+          'py-1.5': size === SfSelectSize.sm,
+          'py-2': size === SfSelectSize.base,
+          'py-3 text-base': size === SfSelectSize.lg,
+          '!ring-negative-700 ring-2': invalid && !disabled,
         },
-      ]" data-testid="select-placeholder">
+      ]"
+      data-testid="select-input"
+      v-bind="$attrs"
+      @blur="close"
+      @change="close"
+      @click="open"
+      @keydown.space="open"
+    >
+      <option
+        v-if="placeholder"
+        disabled
+        hidden
+        class="text-sm bg-neutral-300"
+        value=""
+        :class="[
+          'bg-neutral-300 text-sm',
+          {
+            'text-base': size === SfSelectSize.lg,
+          },
+        ]"
+        data-testid="select-placeholder"
+      >
         {{ placeholder }}
       </option>
       <slot />
     </select>
     <slot name="chevron">
-      <SfIconExpandMore :class="[
-        'absolute -translate-y-1 pointer-events-none top-1/3 right-4 transition easy-in-out duration-0.5',
-        disabled ? 'text-disabled-500' : 'text-neutral-500',
-        isOpen ? 'rotate-180' : '',
-      ]" />
-      <SfIconExpandMore :class="[
-        'absolute -translate-y-1 pointer-events-none top-1/3 right-4 transition easy-in-out duration-0.5',
-        disabled ? 'text-disabled-500' : 'text-neutral-500',
-        isOpen ? 'rotate-180' : '',
-      ]" />
+      <SfIconExpandMore
+        :class="[
+          'absolute -translate-y-1 pointer-events-none top-1/3 right-4 transition easy-in-out duration-0.5',
+          disabled ? 'text-disabled-500' : 'text-neutral-500',
+          isOpen ? 'rotate-180' : '',
+        ]"
+      />
+      <SfIconExpandMore
+        :class="[
+          'absolute -translate-y-1 pointer-events-none top-1/3 right-4 transition easy-in-out duration-0.5',
+          disabled ? 'text-disabled-500' : 'text-neutral-500',
+          isOpen ? 'rotate-180' : '',
+        ]"
+      />
     </slot>
   </span>
 </template>
