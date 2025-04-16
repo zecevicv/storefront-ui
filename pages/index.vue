503baf813334572f95eb4c4cfe435dcfe0731134
Leonardo Muniz <lmuniz@odoogap.com>
2025-04-16 17:17:54 -0300

    style: 💄 better developer warning

--- pages/index.vue
+++ pages/index.vue
@@ -11,15 +11,29 @@ useHead(generateSeo<SeoEntity>(websiteHomepage.value, 'Home'))
 </script>
 
 <template>
-  <MainBanner />
-  <LazyDisplay hydrate-on-visible />
-  <section class="pb-16">
-    <LazyProductSlider key="inspired-by-picks" heading="Inspired by your picks" key-for-composable="inspired-by-picks"
-      hydrate-on-visible />
-  </section>
-  <section v-if="list?.length > 0" class="pb-16">
-    <ClientOnly>
-      <LazyProductSlider key="recent-views" heading="Your recent views" :ids="list" key-for-composable="recent-views" />
-    </ClientOnly>
-  </section>
+  <div>
+    <MainBanner />
+    <LazyDisplay hydrate-on-visible />
+    <section class="pb-16">
+      <LazyProductSlider
+        key="inspired-by-picks"
+        heading="Inspired by your picks"
+        key-for-composable="inspired-by-picks"
+        hydrate-on-visible
+      />
+    </section>
+    <section
+      v-if="list?.length > 0"
+      class="pb-16"
+    >
+      <ClientOnly>
+        <LazyProductSlider
+          key="recent-views"
+          heading="Your recent views"
+          :ids="list"
+          key-for-composable="recent-views"
+        />
+      </ClientOnly>
+    </section>
+  </div>
 </template>
