badbeb88174621058da50eeb4117d9a4e0d24e4e
Rafael Barrozo <barrozorcb@gmail.com>
2025-05-16 16:42:17 -0300

    feat: include a recent view product carousel on cart page

--- server/mutations/CartAddItem.ts
+++ server/mutations/CartAddItem.ts
@@ -1,10 +1,12 @@
 
+import frequentlyTogetherProductsFragment from '../fragments/frequentlyTogetherProductsFragment';
 import orderFragment from '../fragments/orderFragment'
 
 export default `
   mutation($products: [ProductInput]!){
     cartAddMultipleItems(products: $products){
       ${orderFragment}
+      ${frequentlyTogetherProductsFragment}
     }
   }
 `
