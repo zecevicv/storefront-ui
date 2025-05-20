cabcf4ac95920efb3f516a6ff7a8bfbac612f8c2
soareslssa <soareslssa.dev@gmail.com>
2025-05-20 15:54:46 -0300

    feat: 🎉 enhance wishlist button with ClientOnly wrapper

--- domains/core/components/header/ButtonWishlist.vue
+++ domains/core/components/header/ButtonWishlist.vue
@@ -15,23 +15,46 @@ onMounted(async () => {
 </script>
 
 <template>
-  <SfButton
-    class="group relative text-white hover:text-white active:text-white hover:bg-primary-800 active:bg-primary-900 mr-1 -ml-0.5 rounded-md"
-    type="button"
-    variant="tertiary"
-    square
-    @click="handleOpenWishListSidebar"
-  >
-    <template #prefix>
-      <Icon
-        :name="wishlistTotalItems > 0 ? 'mdi:heart' : 'mdi:heart-outline'"
-        size="22px"
-      />
-      <SfBadge
-        :content="wishlistTotalItems"
-        class="outline outline-primary-700 bg-white !text-neutral-900 group-hover:outline-primary-800 group-active:outline-primary-900 flex justify-center"
-        data-testid="wishlist-badge"
-      />
+  <ClientOnly>
+    <SfButton
+      class="group relative text-white hover:text-white active:text-white hover:bg-primary-800 active:bg-primary-900 mr-1 -ml-0.5 rounded-md"
+      type="button"
+      variant="tertiary"
+      square
+      @click="handleOpenWishListSidebar"
+    >
+      <template #prefix>
+        <Icon
+          :name="wishlistTotalItems > 0 ? 'mdi:heart' : 'mdi:heart-outline'"
+          size="22px"
+        />
+        <SfBadge
+          :content="wishlistTotalItems"
+          class="outline outline-primary-700 bg-white !text-neutral-900 group-hover:outline-primary-800 group-active:outline-primary-900 flex justify-center"
+          data-testid="wishlist-badge"
+        />
+      </template>
+    </SfButton>
+
+    <template #fallback>
+      <SfButton
+        class="group relative text-white hover:text-white active:text-white hover:bg-primary-800 active:bg-primary-900 mr-1 -ml-0.5 rounded-md"
+        type="button"
+        variant="tertiary"
+        square
+      >
+        <template #prefix>
+          <Icon
+            name="mdi:heart-outline"
+            size="22px"
+          />
+          <SfBadge
+            :content="0"
+            class="outline outline-primary-700 bg-white !text-neutral-900 group-hover:outline-primary-800 group-active:outline-primary-900 flex justify-center"
+            data-testid="wishlist-badge"
+          />
+        </template>
+      </SfButton>
     </template>
-  </SfButton>
+  </ClientOnly>
 </template>
