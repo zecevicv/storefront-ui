ede108b35ec01f0bd06c5bf632d12099f9252b17
Lucas Soares <soareslssa.dev@gmail.com>
2025-06-11 22:20:56 -0300

    refactor: update route type mapping for website pages in route resolver

--- server/api/route-resolver/[slug].ts
+++ server/api/route-resolver/[slug].ts
@@ -17,7 +17,7 @@ export default defineEventHandler(async (event) => {
       const modelToRouteType = {
         'product.template': 'product',
         'product.public.category': 'category',
-        'vsf.website.page': 'staticPage',
+        'vsf.website.page': 'websitePage',
       }
 
       return {
