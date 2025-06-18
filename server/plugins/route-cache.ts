35589bd7dd5c9d098d5675cc471452f6ae211757
nettoinf <nettoinf@fab.mil.br>
2025-06-18 09:19:20 -0300

    refactor: improve route caching logic and skip routes handling

--- server/plugins/route-cache.ts
+++ server/plugins/route-cache.ts
@@ -1,6 +1,28 @@
 import generateFlags from '@nuxtjs/device/runtime/generateFlags'
 import type { EventHandler, RouterMethod, H3Event } from 'h3'
 
+const routesToSkipCache = [
+  '/api/odoo/all',
+  '/api/odoo/query-no-cache',
+  '/api/route-resolver',
+  '/api/sitemap/urls/categories',
+  '/api/sitemap/urls/products',
+  '/web/health',
+  '/__nuxt_error',
+  '',
+  '/__site-config__/debug.json',
+  '/robots.txt',
+  '/__robots__/debug.json',
+  '/__robots__/debug-path.json',
+  '/__sitemap__/debug.json',
+  '/__sitemap__/style.xsl',
+  '/sitemap.xml',
+  '/__nuxt_island/**',
+  '/_ipx/**',
+  '/_scripts/**',
+  '/**',
+]
+
 type Handler = {
   route: string
   handler: () => EventHandler | Promise<EventHandler>
@@ -10,36 +32,47 @@ type Handler = {
 
 export default defineNitroPlugin((nitroApp) => {
   const handlerList: Handler[] = eval('handlers')
-  const enHandler = handlerList.find((r) => {
-    return (
-      r.route === '/' || r.route === '/product/*' || r.route === '/category/*'
-    )
+
+  const skipRoutesSet = new Set(routesToSkipCache)
+
+  const enHandler = handlerList.filter((r) => {
+    const isRouteToSkip = skipRoutesSet.has(r.route)
+
+    return !isRouteToSkip
   })
 
-  if (enHandler) {
-    const customHandler = cachedEventHandler(
-      lazyEventHandler(enHandler.handler),
-      {
-        varies: ['user-agent'],
-        group: 'pages',
-        name: enHandler.route,
-        getKey: (event: H3Event) => {
-          const headers = getRequestHeaders(event)
-          const userAgent: any = headers['user-agent']
-          const flags = generateFlags(headers, userAgent)
-          if (flags.isDesktop) {
-            return `desktop-${event.path}`
-          }
-          return `mobile-${event.path}`
+  if (enHandler.length > 0) {
+    enHandler.forEach((handler) => {
+      const customHandler = cachedEventHandler(
+        lazyEventHandler(handler.handler),
+        {
+          varies: ['user-agent', 'cookie'],
+          group: 'pages',
+          name: handler.route,
+          getKey: (event: H3Event) => {
+            const headers = getRequestHeaders(event)
+            const userAgent: any = headers['user-agent']
+            const countryIsoCode: any = getCookie(event, 'country-iso-code') || 'US'
+
+            const flags = generateFlags(headers, userAgent)
+
+            if (flags.isDesktop) {
+              return `desktop-${countryIsoCode}-${event.path}`
+            }
+            return `mobile-${countryIsoCode}-${event.path}`
+          },
+          shouldInvalidateCache: (event: H3Event) => {
+            return false
+          },
+          maxAge: Number(process.env?.NUXT_SWR_CACHE_TIME),
+          swr: true,
+          staleMaxAge: Number(process.env?.NUXT_SWR_CACHE_TIME),
+          shouldBypassCache: (event: H3Event) => {
+            return false
+          },
         },
-        shouldInvalidateCache: (event: H3Event) => {
-          return false
-        },
-        shouldBypassCache: (event: H3Event) => {
-          return false
-        },
-      },
-    )
-    nitroApp.router.use(enHandler.route, customHandler)
+      )
+      nitroApp.router.use(handler.route, customHandler)
+    })
   }
 })
