c78bed5fe6d588e538c9f65cac8d91c5309ad9ce
nettoinf <nettoinf@fab.mil.br>
2025-07-02 12:38:42 -0300

    fix: ✨ add frequentlyTogetherProductsFragment to cart mutation responses for improved product recommendations

--- server/mutations/CartRemoveItem.ts
+++ server/mutations/CartRemoveItem.ts
@@ -1,10 +1,11 @@
-
+import frequentlyTogetherProductsFragment from '../fragments/frequentlyTogetherProductsFragment'
 import orderFragment from '../fragments/orderFragment'
 
 export default `
   mutation($lineIds: [Int]!){
     cartRemoveMultipleItems(lineIds: $lineIds) {
       ${orderFragment}
+      ${frequentlyTogetherProductsFragment}
     }
   }
 `
--- server/mutations/CartUpdateQuantity.ts
+++ server/mutations/CartUpdateQuantity.ts
@@ -1,10 +1,11 @@
-
+import frequentlyTogetherProductsFragment from '../fragments/frequentlyTogetherProductsFragment'
 import orderFragment from '../fragments/orderFragment'
 
 export default `
 mutation($lines: [CartLineInput]!){
   cartUpdateMultipleItems(lines: $lines) {
       ${orderFragment}
+      ${frequentlyTogetherProductsFragment}
     }
   }
 `
