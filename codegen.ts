6af99554fa01735624486b54f92577d77c64caad
inNetto <inetto.ferreira@gmail.com>
2025-07-04 10:49:51 -0300

    fix: ✨ update Odoo base URL in environment files for consistency

--- codegen.ts
+++ codegen.ts
@@ -4,7 +4,7 @@ const config: CodegenConfig = {
   overwrite: true,
   schema: [
     {
-      'https://vsf-odoo18.labs.erpgap.com/graphql/vsf': {},
+      'https://vsf-odoo18-dev.labs.erpgap.com/': {},
     },
   ],
   config: {
