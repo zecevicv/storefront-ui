d7afa9fdd82f0c8b71578ca01392731b13dd6a57
inNetto <inetto.ferreira@gmail.com>
2025-07-20 14:19:29 -0300

    Rollback using domains product recent-views instead module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: [
-    '@erpgap/recent-view-products',
-  ],
   modules: [
     '@pinia/nuxt',
     '@nuxtjs/tailwindcss',
